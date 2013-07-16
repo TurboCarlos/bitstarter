cb3b758284f60579a235abdb99a4773a6107d2bb
TurboCarlos <turbocarlos@gmail.com>
2013-07-16 05:16:13 +0000

    Read index.html

--- web.js
+++ web.js
@@ -1,9 +1,23 @@
+var fs = require('fs');
 var express = require('express');
 
+//Get site contents
+
+var page = 'index.html';
+
+var content = function(page) { 
+	return fs.readFile(page, function (err, data) {
+		if (err) throw err;
+		var buf = new Buffer(data)
+		buf.toString("utf8",0,buf.length);
+	});
+};
+
+//Output to server
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
